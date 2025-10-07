38fbdc684743cab0354e7e081ec5def205e30db6
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-07 11:02:37 -0300

    feat: Add AppTextStyles class with various text styles for consistent typography based on figma.

---
+++ lib/utils/design/app_text_styles.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle manropeBold14 = TextStyle(
+    fontFamily: 'Manrope',
+    fontWeight: FontWeight.w700,
+    fontSize: 14.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle manropeSemiBold12 = TextStyle(
+    fontFamily: 'Manrope',
+    fontWeight: FontWeight.w600,
+    fontSize: 12.0,
+    height: 16.0 / 12.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle manropeRegular16 = TextStyle(
+    fontFamily: 'Manrope',
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle manropeRegular16LineHeight24 = TextStyle(
+    fontFamily: 'Manrope',
+    fontWeight: FontWeight.w400,
+    fontSize: 16.0,
+    height: 24.0 / 16.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishBold16 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    fontSize: 16.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishBold16LineHeight20 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    fontSize: 16.0,
+    height: 20.0 / 16.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishSemiBold16 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w600,
+    fontSize: 16.0,
+    height: 1.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishBold9 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    fontSize: 9.0,
+    height: 11.0 / 9.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishBold12 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    fontSize: 12.0,
+    height: 1.5,
+    letterSpacing: 0.3,
+  );
+
+  static const TextStyle mulishBold12LineHeight20 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w700,
+    fontSize: 12.0,
+    height: 20.0 / 12.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishRegular10 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    fontSize: 10.0,
+    height: 16.0 / 10.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishRegular10Simple = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    fontSize: 10.0,
+  );
+
+  static const TextStyle mulishRegular8 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    fontSize: 8.0,
+    height: 20.0 / 8.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishRegular14 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    fontSize: 14.0,
+    height: 24.0 / 14.0,
+    letterSpacing: 0.0,
+  );
+
+  static const TextStyle mulishSemiBold17 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w600,
+    fontSize: 17.0,
+    height: 9.79 / 17.0,
+    letterSpacing: -0.41,
+  );
+
+  static const TextStyle mulishRegular17 = TextStyle(
+    fontFamily: 'Mulish',
+    fontWeight: FontWeight.w400,
+    fontSize: 17.0,
+    height: 9.79 / 17.0,
+    letterSpacing: -0.41,
+  );
+}
