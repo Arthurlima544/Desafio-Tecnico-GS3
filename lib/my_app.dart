01e3e5aec81b0729d77ae839000e3ecafa043736
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-07 14:01:50 -0300

    feat: Implement main application structure

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'utils/design/design.dart';
+import 'view/login/login.dart';
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => const Placeholder();
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'gs3_tecnologia',
+    theme: lightTheme(),
+    debugShowCheckedModeBanner: false,
+    home: const Login(),
+  );
 }
