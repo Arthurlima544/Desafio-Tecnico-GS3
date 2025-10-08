cfe80f87c64c47952d54f55faa0bd25f6ec5b4ae
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-08 11:32:47 -0300

    feat: enhance LoginModal responsiveness with cpf and password validation.

--- lib/view/login/widgets/login_modal.dart
+++ lib/view/login/widgets/login_modal.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../utils/design/design.dart';
+import '../../../view_model/login/login_view_model.dart';
 
 class LoginModal extends StatefulWidget {
   const LoginModal({required this.onClose, super.key});
@@ -12,105 +14,157 @@ class LoginModal extends StatefulWidget {
 }
 
 class _LoginModalState extends State<LoginModal> {
+  late final TextEditingController _cpfController;
+  late final TextEditingController _passwordController;
+
   bool _isPasswordVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _cpfController = TextEditingController();
+    _passwordController = TextEditingController();
+
+    final LoginViewModel viewModel = context.read<LoginViewModel>();
+
+    _cpfController.addListener(() {
+      viewModel.onCpfChanged(_cpfController.text);
+    });
+
+    _passwordController.addListener(() {
+      viewModel
+        ..onPasswordChanged(_passwordController.text)
+        ..validateCredentials();
+    });
+  }
+
+  @override
+  void dispose() {
+    _cpfController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => Container(
     padding: EdgeInsets.all(Theme.of(context).extension<AppSpacings>()!.large),
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        const Text('Seja bem-vindo(a)!', style: AppTextStyles.mulishBold16),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.medium),
-        const Text(
-          'Digite sua senha do aplicativo.',
-          style: AppTextStyles.manropeRegular16LineHeight24,
-        ),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.large),
-        TextFormField(
-          style: AppTextStyles.manropeRegular16LigthGray,
-          textInputAction: TextInputAction.next,
-          decoration: InputDecoration(
-            labelText: 'CPF',
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
+    child: ListenableBuilder(
+      listenable: context.watch<LoginViewModel>(),
+      builder: (BuildContext context, Widget? child) {
+        final LoginViewModel loginViewModel = context.read<LoginViewModel>();
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('Seja bem-vindo(a)!', style: AppTextStyles.mulishBold16),
+            SizedBox(
+              height: Theme.of(context).extension<AppSpacings>()!.medium,
             ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: const BorderSide(color: AppColors.ligthergray),
+            const Text(
+              'Digite sua senha do aplicativo.',
+              style: AppTextStyles.manropeRegular16LineHeight24,
             ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: const BorderSide(color: AppColors.primaryVariant),
-            ),
+            SizedBox(height: Theme.of(context).extension<AppSpacings>()!.large),
+            TextFormField(
+              controller: _cpfController,
+              style: AppTextStyles.manropeRegular16LigthGray,
+              textInputAction: TextInputAction.next,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: loginViewModel.validateCpf,
+              decoration: InputDecoration(
+                labelText: 'CPF',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: AppColors.ligthergray),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: AppColors.primaryVariant),
+                ),
 
-            hintText: 'Digite seu CPF',
-          ),
-          keyboardType: TextInputType.number,
-        ),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.medium),
-        TextFormField(
-          obscureText: !_isPasswordVisible,
-          style: AppTextStyles.manropeRegular16LigthGray,
-          textInputAction: TextInputAction.done,
-          decoration: InputDecoration(
-            labelText: 'Senha',
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            hintText: 'Digite sua senha',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: const BorderSide(color: AppColors.primaryVariant),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(16.0),
-              borderSide: const BorderSide(color: AppColors.ligthergray),
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(
-                _isPasswordVisible
-                    ? AppIcons.visibilityOutlined
-                    : AppIcons.visibilityOffOutlined,
-                color: Colors.grey,
+                hintText: 'Digite seu CPF',
               ),
-              onPressed: () {
-                setState(() {
-                  _isPasswordVisible = !_isPasswordVisible;
-                });
-              },
+              keyboardType: TextInputType.number,
             ),
-          ),
-        ),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.large),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.small),
-        SizedBox(
-          width: double.infinity,
-          child: ElevatedButton(
-            onPressed: () {
-              FocusScope.of(context).unfocus();
-              widget.onClose();
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: AppColors.primary,
-              padding: EdgeInsets.symmetric(
-                vertical: Theme.of(context).extension<AppSpacings>()!.medium,
-              ),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
+            SizedBox(
+              height: Theme.of(context).extension<AppSpacings>()!.medium,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              obscureText: !_isPasswordVisible,
+              style: AppTextStyles.manropeRegular16LigthGray,
+              textInputAction: TextInputAction.done,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              validator: loginViewModel.validatePassword,
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                hintText: 'Digite sua senha',
+
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: AppColors.primaryVariant),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                  borderSide: const BorderSide(color: AppColors.ligthergray),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? AppIcons.visibilityOutlined
+                        : AppIcons.visibilityOffOutlined,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
               ),
             ),
-            child: const Text(
-              'Confirmar',
-              style: AppTextStyles.mulishSemiBold16,
+            SizedBox(height: Theme.of(context).extension<AppSpacings>()!.large),
+            SizedBox(height: Theme.of(context).extension<AppSpacings>()!.small),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: loginViewModel.isFormValid
+                    ? () {
+                        FocusScope.of(context).unfocus();
+                        widget.onClose();
+                      }
+                    : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primary,
+                  padding: EdgeInsets.symmetric(
+                    vertical: Theme.of(
+                      context,
+                    ).extension<AppSpacings>()!.medium,
+                  ),
+                  disabledBackgroundColor: AppColors.lightWhite,
+
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+                child: const Text(
+                  'Confirmar',
+                  style: AppTextStyles.mulishSemiBold16,
+                ),
+              ),
             ),
-          ),
-        ),
-        SizedBox(height: Theme.of(context).extension<AppSpacings>()!.small),
-      ],
+            SizedBox(height: Theme.of(context).extension<AppSpacings>()!.small),
+          ],
+        );
+      },
     ),
   );
 }
