15a85faed6a775ad65d72a803e7c2750edd949ea
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-09 09:00:23 -0300

    feat: add CardEntity

---
+++ lib/domain/entities/card_entity.dart
@@ -0,0 +1,15 @@
+final class CardEntity {
+  const CardEntity({
+    required this.uuid,
+    required this.cardNumber,
+    required this.cardHolder,
+    required this.limit,
+    required this.bestDay,
+  });
+
+  final String uuid;
+  final String cardNumber;
+  final String cardHolder;
+  final String limit;
+  final String bestDay;
+}
