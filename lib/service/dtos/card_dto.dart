1208d973817068295ddac88ccc0e85a7d1716ee2
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-09 09:13:13 -0300

    feat: add CardDTO

---
+++ lib/service/dtos/card_dto.dart
@@ -0,0 +1,29 @@
+final class CardDTO {
+  const CardDTO({
+    required this.uuid,
+    required this.cardNumber,
+    required this.cardHolder,
+    required this.limit,
+    required this.bestDay,
+    required this.expirationDate,
+    required this.cvv,
+  });
+
+  factory CardDTO.fromJson(Map<String, dynamic> json) => CardDTO(
+    uuid: json['uuid'],
+    cardNumber: json['cardNumber'],
+    cardHolder: json['cardHolder'],
+    limit: json['limit'].toDouble(),
+    bestDay: json['bestDay'],
+    cvv: json['cvv'],
+    expirationDate: json['expirationDate'],
+  );
+
+  final String uuid;
+  final String cardNumber;
+  final String cardHolder;
+  final String expirationDate;
+  final String cvv;
+  final String limit;
+  final String bestDay;
+}
