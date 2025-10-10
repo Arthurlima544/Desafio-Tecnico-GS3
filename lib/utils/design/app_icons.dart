fe5ad453dd1688b09115308667660569bffdd62c
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-10-10 11:45:41 -0300

    hotfix: tree shake error avoiding non-constant invocations of IconData

--- lib/utils/design/app_icons.dart
+++ lib/utils/design/app_icons.dart
@@ -21,6 +21,21 @@ class AppIcons {
   static const IconData visibilityOffOutlined = Icons.visibility_off_outlined;
   static const IconData receipt = Icons.receipt_long_outlined;
 
+  static const Map<int, IconData> _iconMap = <int, IconData>{
+    0xe541: Icons.shopping_cart,
+    0xe8cc: Icons.supervisor_account,
+    0xef50: Icons.qr_code_scanner,
+    0xe325: Icons.house,
+    0xe531: Icons.receipt,
+    0xe40f: Icons.monetization_on,
+    0xe030: Icons.assessment,
+    0xe546: Icons.store,
+    0xea68: Icons.payments,
+    0xe54e: Icons.sync,
+    0xe8a1: Icons.person,
+    0xe865: Icons.inventory,
+  };
+
   static IconData getIconByCodePoint(int codePoint) =>
-      IconData(codePoint, fontFamily: 'MaterialIcons');
+      _iconMap[codePoint] ?? Icons.help_outline;
 }
